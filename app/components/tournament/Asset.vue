52e9f14606ac2d364ec170f3dc1230a6c62df6c4
kirbytwister <a.osuskiy@gmail.com>
2026-06-01 23:50:56 +0200

    asset component

---
+++ app/components/tournament/Asset.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { capitalize } from "vue";
+
+import { cn } from "~/lib/utils";
+
+defineProps<{
+  img: string | null;
+  type: "logo" | "banner";
+}>();
+</script>
+
+<template>
+  <Card
+    :class="
+      cn('bg-background flex h-full max-h-80 w-full', type === 'logo' ? 'max-w-72' : 'max-w-132')
+    "
+  >
+    <CardHeader>
+      <h1 class="text-2xl">{{ capitalize(type) }}</h1>
+    </CardHeader>
+    <CardContent class="flex justify-center">
+      <div
+        class="h-48"
+        :style="{
+          aspectRatio: type === 'logo' ? '1/1' : '21/9',
+        }"
+      >
+        <img v-if="img" :src="img" class="h-full w-full object-cover" />
+        <Empty class="bg-card size-full select-none" v-else>
+          <EmptyHeader>
+            <EmptyMedia>
+              <Icon name="lucide:image-off" size="48" />
+            </EmptyMedia>
+            <EmptyDescription>No image has been uploaded</EmptyDescription>
+          </EmptyHeader>
+        </Empty>
+      </div>
+    </CardContent>
+  </Card>
+</template>
--- app/pages/[slug]/manage/assets.vue
+++ app/pages/[slug]/manage/assets.vue
@@ -1,1 +1,8 @@
-<template>ASSETS</template>
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex max-w-screen gap-3 overflow-visible">
+    <TournamentAsset :img="null" type="logo" />
+    <TournamentAsset :img="null" type="banner" />
+  </div>
+</template>
